d5958cb3f8493568bbea44077b7e91434fe83547
bepinyd <bepinyd@gmail.com>
2025-10-02 13:41:03 +0530

    None

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
 app.use("/bikeImages" , express.static(path.join(process.cwd() , "./bikeImages")))
-app.use(cors({origin : 'http://localhost:3000' , credentials : true}))
+app.use(cors({origin : process.env.CLIENT_URL , credentials : true}))
 app.use("/api/auth" , authRoute)
 app.use("/api/auth/bikes" , bikeRoute)
 app.listen( PORT , async ()=>{
